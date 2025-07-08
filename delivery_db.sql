e006c2b7fd5f817e7f35f1c151d50ab030912f99
cbastiaaanz20 <c.bastianz72@gmail.com>
2025-07-08 00:22:41 -0500

    Update delivery_db.sql

--- delivery_db.sql
+++ delivery_db.sql
@@ -51,3 +51,45 @@ unitary_price DECIMAL(10,2) NOT NULL,
 FOREIGN KEY (order_id) REFERENCES orders(id),
 FOREIGN KEY (product_id) REFERENCES products(id)
 );
+
+INSERT INTO USER (name, email, phone, address) VALUES
+('Mariana Paez', 'mariana.p@email.com', '315-1112233', 'Calle 80 # 70-30'),
+('Ricardo Solis', 'ricardo.s@email.com', '316-4445566', 'Diagonal 45 # 12-34'),
+('Valeria Luna', 'valeria.l@email.com', '317-7778899', 'Carrera 19 # 120-50, Apto 502');
+
+INSERT INTO restaurant (name, address, phone) VALUES
+('Pizza Paraíso', 'Calle Falsa 123, Ciudad Capital', '555-0101'),
+('Sushi Express', 'Avenida Siempre Viva 456, Villa Sabor', '555-0102'),
+('Taco Fiesta', 'Plaza Central 789, Pueblo Delicia', '555-0103');
+
+INSERT INTO Products (restaurant_id, name, description, price) VALUES
+(1, 'Pizza Hawaiana', 'Pizza con piña, jamón y queso mozzarella.', 25.50),
+(1, 'Pizza Pepperoni', 'Clásica pizza de pepperoni con extra queso.', 22.00),
+(1, 'Lasaña de Carne', 'Capas de pasta con salsa boloñesa y bechamel.', 28.00);
+
+-- Productos de Sushi Express (restaurante_id = 2)
+INSERT INTO Products (restaurant_id, name, description, price) VALUES
+(2, 'Rollo California', 'Rollo de cangrejo, aguacate y pepino.', 18.00),
+(2, 'Nigiri de Salmón', 'Finas lonjas de salmón sobre arroz de sushi.', 15.50),
+(2, 'Yakimeshi Mixto', 'Arroz frito con verduras, pollo y camarón.', 21.00);
+
+-- Productos de Taco Fiesta (restaurante_id = 3)
+INSERT INTO Products (restaurant_id, name, description, price) VALUES
+(3, 'Tacos al Pastor', 'Tres tacos de cerdo marinado con piña.', 16.00),
+(3, 'Quesadilla de Champiñones', 'Tortilla de harina con queso y champiñones.', 14.00),
+(3, 'Burrito de Carne Asada', 'Gran burrito con carne, frijoles y arroz.', 20.50);
+
+INSERT INTO orders (user_id, restaurant_id, estatus) VALUES
+(2, 3, 'En preparacion'); 
+
+SET @ID_ORDER = LAST_INSERT_ID();
+
+INSERT INTO order_details (order_id, product_id, quantity, unitary_price) VALUES 
+(@ID_ORDER, 7, 2, 16.00),
+(@ID_ORDER, 8, 1, 14.00);
+ 
+-- Valeria Luna (user_id =  3) ha cambiado su numero de telefono 
+UPDATE user SET phone = "32154863" WHERE id = 3;
+
+-- El precio de Pizza Hawaiana ha subido.
+UPDATE Products SET price = 5000 WHERE name = "Pizza Hawaina";
